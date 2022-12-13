4b86dd73dcd7484ed1c53b499756157ca155ecf3
Emir Cem Gezer <emircem.gezer@gmail.com>
2022-12-13 16:08:25 -0500

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Alpha Core AUV
+
+This repository contains source codes for ALPHA AUV family.
+
+ALPHA Science AUV
+![alpha sci picture](docs/images/alpha_sci.png)
+
+ALPHA Imaging AUV
+![alpha img picutre](docs/images/alpha_img.png)
+
+## Directory Structure
+
+`alpha_driver`
+Driver package for the micro controller inside the ALPHA AUVs.
+
+`alpha_localization`
+Localization configuration for ALPHA AUVs.
+
+## Installation
+
+### Clone the repository
+
+```bash
+git clone --single-branch --branch noetic-devel https://github.com/uri-ocean-robotics/alpha_core
+```
+
+### MVP Installation
+
+Currently MVP packages should be build from the source.
+Target platform must be Ubuntu 20.04 because of the dependencies.
+
+Pull repository and other dependencies
+```bash
+git clone --single-branch --branch noetic-devel https://github.com/uri-ocean-robotics/mvp_msgs
+```
+
+### Install Dependencies
+
+Install dependencies
+```bash
+rosdep install --from-paths src --ignore-src --rosdistro ${ROS_DISTRO} -y
+```
+
+## Citation
+
+If you find this software useful in your research, please cite:
+
+> Note, this work is published in OCEANS 2022 conference. Once the paper is publicly available, bibtex entry
+will be updated with the one from IEEExplore.
+
+```
+@inproceedings{
+    ALPHA_PAPER,
+    title = {Acrobatic Low-cost Portable Hybrid AUV (ALPHA): System Design and Preliminary Results},
+    author={Zhou, Mingxi and Gezer, Emir Cem and McConnell, William and Yuan, Chengzhi},
+    booktitle={OCEANS 2022: Hampton Roads},
+    year={2022},
+    organization={IEEE}
+}
+```
+
+## Funding
+This work is supported by the [National Science Foundation](https://www.nsf.gov/) award [#2154901](https://www.nsf.gov/awardsearch/showAward?AWD_ID=2154901&HistoricalAwards=false)
---
+++ alpha_localization/src/depth_filter_node.cpp
@@ -0,0 +1,83 @@
+/*
+    This file is part of ALPHA AUV project.
+
+    This project is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This project is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the project.  If not, see <https://www.gnu.org/licenses/>.
+
+    Author: Emir Cem Gezer
+    Email: emircem@uri.edu;emircem.gezer@gmail.com
+    Year: 2022
+
+    Copyright (C) 2022 Smart Ocean Systems Laboratory
+*/
+
+#include "depth_filter_node.hpp"
+
+DepthFilterNode::DepthFilterNode() {
+    m_nh.reset(new ros::NodeHandle(""));
+    m_pnh.reset(new ros::NodeHandle("~"));
+
+
+    double A, B, H, Q, R, P, x;
+
+    m_pnh->param<double>("A", A, 1.0);
+    m_pnh->param<double>("B", B, 0.0);
+    m_pnh->param<double>("H", H, 1.0);
+    m_pnh->param<double>("Q", Q, 1e-4);
+    m_pnh->param<double>("R", R, 1e-4);
+    m_pnh->param<double>("P", P, 1.0);
+    m_pnh->param<double>("x", x, 0.0);
+
+
+    m_pnh->param<std::string>("frame_id", m_frame_id, "world");
+
+    m_filter.reset(new UnoLqe(x, A, B, H, Q, R, P));
+
+    m_depth_subscriber = m_nh->subscribe("depth", 10, &DepthFilterNode::f_depth_callback, this);
+    m_depth_publisher = m_nh->advertise<geometry_msgs::PoseWithCovarianceStamped>("depth_filtered", 10);
+
+}
+
+void DepthFilterNode::f_depth_callback(const mvp_msgs::Float64StampedConstPtr &msg) {
+
+    m_filter->predict();
+    m_filter->update(msg->data);
+
+    double x, p;
+    m_filter->state(&x, &p);
+
+    geometry_msgs::PoseWithCovarianceStamped out;
+
+    out.header = msg->header;
+    out.header.frame_id = m_frame_id;
+    out.pose.pose.position.x = 0.0;
+    out.pose.pose.position.y = 0.0;
+    out.pose.pose.position.z = -x;
+
+    out.pose.covariance[6 * 2 + 2] = p;
+
+    m_depth_publisher.publish(out);
+
+}
+
+
+int main(int argc, char* argv[]) {
+
+    ros::init(argc, argv, "depth_filter");
+
+    DepthFilterNode d;
+
+    ros::spin();
+
+    return 0;
+}
