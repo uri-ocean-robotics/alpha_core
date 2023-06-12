fb7845c5d72be2d6de8ead538c0c2b5982a30892
Lin Zhao <linzhao@uri.edu>
2023-06-12 21:38:22 +0000

    update thruster test for mapping joy to pwm topic

--- alpha_driver/src/alpha_driver_uart/test/test_thruster.cpp
+++ alpha_driver/src/alpha_driver_uart/test/test_thruster.cpp
@@ -13,10 +13,10 @@ public:
             :nh_(nh), nh_private_(nh_private)
     {
         sub = nh_.subscribe<sensor_msgs::Joy> ("/joy", 10, &Testthruster::CallbackJoy, this);
-        pub0 = nh_.advertise<std_msgs::Float64>("control/pwm_chan0", 10);
-        pub1 = nh_.advertise<std_msgs::Float64>("control/pwm_chan1", 10);
-        pub2 = nh_.advertise<std_msgs::Float64>("control/pwm_chan2", 10);
-        pub3 = nh_.advertise<std_msgs::Float64>("control/pwm_chan3", 10);
+        pub0 = nh_.advertise<std_msgs::Float64>("joy_axes0", 10);
+        pub1 = nh_.advertise<std_msgs::Float64>("joy_axes1", 10);
+        pub2 = nh_.advertise<std_msgs::Float64>("joy_axes2", 10);
+        pub3 = nh_.advertise<std_msgs::Float64>("joy_axes3", 10);
 
         nh_private_.param<double>("factor", factor, 0.5);
     }
@@ -41,32 +41,32 @@ private:
 
 void Testthruster::CallbackJoy(const sensor_msgs::Joy::ConstPtr& input) {
     
-    auto ch0 = input->axes[0];
-    auto ch1 = input->axes[1];
-    auto ch2 = input->axes[2];
-    auto ch3 = input->axes[3];
+    auto axes0 = input->axes[0];
+    auto axes1 = input->axes[1];
+    auto axes2 = input->axes[2];
+    auto axes3 = input->axes[3];
 
     std::cout<<std::endl;
-    std::cout<<"ch0: " << ch0 << std::endl;
-    std::cout<<"ch1: " << ch1 << std::endl;
-    std::cout<<"ch2: " << ch2 << std::endl;
-    std::cout<<"ch3: " << ch3 << std::endl;
+    std::cout<<"axes0: " << axes0 << std::endl;
+    std::cout<<"axes1: " << axes1 << std::endl;
+    std::cout<<"axes2: " << axes2 << std::endl;
+    std::cout<<"axes3: " << axes3 << std::endl;
 
     // send cmd to pico ros driver
-    std_msgs::Float64 pwm_ch0;
-    std_msgs::Float64 pwm_ch1;
-    std_msgs::Float64 pwm_ch2;
-    std_msgs::Float64 pwm_ch3;
+    std_msgs::Float64 pwm_axes0;
+    std_msgs::Float64 pwm_axes1;
+    std_msgs::Float64 pwm_axes2;
+    std_msgs::Float64 pwm_axes3;
 
-    pwm_ch0.data = ch0*0.5;
-    pwm_ch1.data = ch1*0.5;
-    pwm_ch2.data = ch2*0.5;
-    pwm_ch3.data = ch3*0.5;
+    pwm_axes0.data = axes0*0.5;
+    pwm_axes1.data = axes1*0.5;
+    pwm_axes2.data = axes2*0.5;
+    pwm_axes3.data = axes3*0.5;
 
-    pub0.publish(pwm_ch0);
-    pub1.publish(pwm_ch1);
-    pub2.publish(pwm_ch2);
-    pub3.publish(pwm_ch3);
+    pub0.publish(pwm_axes0);
+    pub1.publish(pwm_axes1);
+    pub2.publish(pwm_axes2);
+    pub3.publish(pwm_axes3);
 }
 
 int main(int argc, char **argv) {
